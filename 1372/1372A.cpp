c0176d68131069c23455afc48c5f8f361772b6dd
SeungHoon Jang <jsh1400512@naver.com>
2020-07-19 21:48:58 +0900

    Commit

---
+++ 1372/1372A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int t, n;
+
+int main() {
+	cin >> t;
+	
+	while(t--) {
+		cin >> n;
+		
+		while(n--) {
+			cout << 1 << " ";
+		}
+		
+		cout << endl;
+	}
+}
